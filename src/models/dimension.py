fdbf16ad6a62bdadb89cf597bb1c2007fafc100a
Your Name <emmanuel.bavoux@gmail.com>
2018-12-15 21:59:45 +0100

    Refacto Category with BaseItem

--- src/models/dimension.py
+++ src/models/dimension.py
@@ -1,7 +1,25 @@
 from src.utils.property_decorators import property_is_int, property_is_string
 
 
-class Category(object):
+class BaseItem(object):
+    """Base class defining an Item
+
+    Parameters
+    ----------
+    id : String
+        Unique ID for the item
+    index : Integer
+        Position of the item within list of similar items.
+    label : String
+        Explicit label for the item.
+
+    Attributes
+    ----------
+    id
+    index
+    label
+
+    """
 
     def __init__(self, id, index, label):
         self.id = id
@@ -34,3 +52,21 @@ class Category(object):
     @property_is_string
     def label(self, value):
         self._label = value
+
+
+class Category(BaseItem):
+    """Represent a category item with a dimension.
+
+    Parameters
+    ----------
+    id : String
+        See BaseItem for details.
+    index : type
+        See BaseItem for details.
+    label : type
+        See BaseItem for details.
+
+    """
+
+    def __init__(self, id, index, label):
+        super().__init__(id, index, label)
