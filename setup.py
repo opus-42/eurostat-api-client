6993acf06269b06fcd3187fd02cba0fe0914ffc5
Your Name <emmanuel.bavoux@gmail.com>
2018-12-27 21:43:31 +0100

    Simple setup configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils import setup
+
+setup(name='eurostat-api-client',
+      version='0.1.0',
+      description='Eurostat REST API PYTHON CLIENT',
+      author='Emmanuel BAVOUX',
+      author_email='emmanuel.bavoux@gmail.com',
+      packages=[''],
+      )
