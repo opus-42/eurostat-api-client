8c1398414d53fcfd4c8177764f4b34e341d95fd6
Your Name <emmanuel.bavoux@gmail.com>
2018-12-27 22:22:04 +0100

    Correct setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from distutils import setup
+import setuptools
 
-setup(name='eurostat-api-client',
-      version='0.1.0',
-      description='Eurostat REST API PYTHON CLIENT',
-      author='Emmanuel BAVOUX',
-      author_email='emmanuel.bavoux@gmail.com',
-      packages=[''],
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eurostat-api-client",
+    version="0.1.0",
+    author="Emmanuel Bavoux",
+    author_email="emmanuel.bavoux@gmail.com",
+    description="A simple Eurostat Rest API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opus-42/eurostat-api-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
