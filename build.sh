132a4d1500d32aa0dd4c5f63a74d0859bf38a1f9
Emmanuel Bavoux <emmanuel.bavoux@gmail.com>
2020-10-07 00:28:44 +0200

    Update build for version release

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 
 python3 -m pip install --user --upgrade twine
-twine upload dist/*
+python3 -m twine upload dist/*
